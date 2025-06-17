2b7b43f813b1aff36628f6b6d5a3ac0e31d76cb5
sungsuhan <swhanssu@gmail.com>
2025-06-17 14:33:14 +0900

    Remove config.py from Git tracking

--- app/core/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    database_url: str = "postgresql://postgres:softworks!12@localhost:5432/consultorang"
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
